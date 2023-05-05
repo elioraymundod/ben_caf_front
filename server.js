a80cb395235490930d1826c5eae7394180963ff7
Elio Raymundo <raymundoelio@gmail.com>
2023-05-05 10:19:41 -0600

    se realizan cambios en server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 //save the static files
-app.use(express.static('./dist/BEN_CAF_FRONT'));
+app.use(express.static('./dist/sistema-inventarios'));
 
 app.get('/*', (req, res) => 
-    res.sendFile('index.html', {root: 'dist/BEN_CAF_FRONT'})
+    res.sendFile('index.html', {root: 'dist/sistema-inventarios'})
 )
 
 //Start the app by listening on the default heroku port
